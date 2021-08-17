02e59d5e5e58a809f7fc6002920ece7bf12c4700
EC2 Default User <ec2-user@ip-172-31-87-193.ec2.internal>
2021-08-17 17:23:07 +0000

    fixing

--- helloworld/application.py
+++ helloworld/application.py
@@ -3,12 +3,13 @@ import json
 from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
 import requests
-from flask_cors import CORS
 import boto3
+from flask_cors import CORS
 from datetime import datetime
+import simplejson as json
 
 application = Flask(__name__)
-CORS(application, resources={r"/": {"origins": ""}})
+CORS(application, resources={r"/*": {"origins": "*"}})
 
 @application.route('/', methods=['GET'])
 def get():
@@ -17,9 +18,7 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
-    
-    
-    
+
 @application.route('/get_cops', methods=['GET'])
 def get_id():
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
@@ -28,27 +27,20 @@ def get_id():
     resp = table.scan()
     print(str(resp))
     return Response(json.dumps(resp['Items']), mimetype='application/json', status=200)
-    
-    
-    
-# curl -i -X POST -d'{"name":"Shiran", "department":"Surgery", "years":"20"}' -H "Content-Type: application/json" http://localhost:8000/set_cop/4
-@application.route('/set_cop/<id>', methods=['POST'])
-def set_cop(id):
+
+# curl -i http://"localhost:8000/set_cop?id=8&name=Dor&age=23&phone=052455&rank=2"
+@application.route('/set_cop', methods=['GET'])
+def set_doc():
     
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('cops')
-    # get post data  
-    data = request.data
-    # convert the json to dictionary
-    data_dict = json.loads(data)
-    # retreive the parameters
-    name = data_dict.get('name','default')
-    age = data_dict.get('age','defualt')
-    phone = data_dict.get('phone', 'default')
-    rank = data_dict.get('rank', 'default')
-
+    cid = request.args.get('id')
+    name = request.args.get('name')
+    age = request.args.get('age')
+    phone = request.args.get('phone')
+    rank = request.args.get('rank')
     item={
-    'id': id,
+    'id': cid,
     'name': name,
     'age': age, 
     'phone': phone,
@@ -57,12 +49,11 @@ def set_cop(id):
     table.put_item(Item=item)
     
     return Response(json.dumps(item), mimetype='application/json', status=200)
-    
 
-#curl -i http://"localhost:8000/delete_cops?id=1"
+#curl -i http://"localhost:5000/del_doctor?id=1"
 @application.route('/del_cop' , methods=['GET'])
 
-def delete_cop():
+def del_doc():
     id=request.args.get('id')
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('cops')
@@ -73,11 +64,9 @@ def delete_cop():
         }
         )
     print (str(resp))
-    return Response(json.dumps(str(resp)), mimetype='application/json', status=200)
-    
-    
-    
-#curl localhost:8000/analyze/my-upload-image/male.jpg  
+    return Response(json.dumps(resp), mimetype='application/json', status=200)
+
+#curl localhost:8000/analyze/doctorspictures/doc1.jpg  
 
 @application.route('/analyze/<bucket>/<image>', methods=['GET'])
 def analyze(bucket='my-upload-image', image='yarden.jpeg'):
@@ -98,7 +87,7 @@ def detect_labels(bucket, key, max_labels=3, min_confidence=90, region="us-east-
     
 
 
-#curl localhost:8000/comp_face/shiran.jpeg/shiran2.jpeg
+#curl localhost:8000/comp_face/doc1.jpg/doc2.jpg
 
 @application.route('/comp_face/<source_image>/<target_image>', methods=['GET'])
 def compare_face(source_image, target_image):
@@ -138,9 +127,6 @@ def uploadImage():
     filename = "%s.jpg" % dt_string
     s3.Bucket(mybucket).upload_fileobj(filobject, filename, ExtraArgs={'ACL': 'public-read', 'ContentType': 'image/jpeg'})
     return {"imgName": filename}
-    
-
-
 
 if __name__ == '__main__':
     flaskrun(application)
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'flask',
         'flask_cors',
+        'simplejson'
     ],
     setup_requires=[
         'pytest-runner',
